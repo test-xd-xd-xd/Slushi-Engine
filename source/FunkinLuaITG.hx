ef3b281fa68ba46aa0c8c32e049575963ec1c452
Andres_Github <lol1232018yt@gmail.com>
2023-02-15 19:54:32 -0400

    Test ITG

---
+++ source/FunkinLuaITG.hx
@@ -0,0 +1,96 @@
+package;
+
+#if ITG_ALLOWED
+import llua.Lua;
+import llua.LuaL;
+import llua.State;
+import llua.Convert;
+//import modchart.*;
+#end
+
+import animateatlas.AtlasFrameMaker;
+import flixel.FlxG;
+import flixel.addons.effects.FlxTrail;
+import flixel.input.keyboard.FlxKey;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+import flixel.text.FlxText;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxPoint;
+import flixel.system.FlxSound;
+import flixel.util.FlxTimer;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.util.FlxColor;
+import flixel.FlxBasic;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import openfl.Lib;
+import openfl.display.BlendMode;
+import openfl.filters.BitmapFilter;
+import openfl.utils.Assets;
+import flixel.math.FlxMath;
+import flixel.util.FlxSave;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.system.FlxAssets.FlxShader;
+
+
+class FunkinLuaITG {
+
+    public var playerNotesFollowtorReceps=true;
+    public var opponentNotesFollowReceptors=true;
+
+    //set('defaultPlayerStrumX' + i, 0);
+    //set('defaultPlayerStrumY' + i, 0);
+    //set('defaultPlayerStrumZ' + i, 0);
+    //set('defaultOpponentStrumX' + i, 0);
+    //set('defaultOpponentStrumY' + i, 0);
+    //set('defaultOpponentStrumZ' + i, 0);
+
+	
+    public function new(script:String) {
+		#if LUA_ALLOWED
+		lua = LuaL.newstate();
+		LuaL.openlibs(lua);
+		Lua.init_callbacks(lua);
+
+        public var scriptName:String = 'modchartTTG';
+
+		//trace('Lua version: ' + Lua.version());
+		//trace("LuaJIT version: " + Lua.versionJIT());
+
+		//LuaL.dostring(lua, CLENSE);
+		try{
+			var result:Dynamic = LuaL.dofile(lua, script);
+			var resultStr:String = Lua.tostring(lua, result);
+			if(resultStr != null && result != 0) {
+				trace('Error loading Lua ITG script!! ' + resultStr);
+				#if windows
+				lime.app.Application.current.window.alert(resultStr, 'Error loading Lua ITG script!!');
+				#else
+				luaTrace('Error loading lua ITG script: "$script"\n' + resultStr, true, false, FlxColor.RED);
+				#end
+				lua = null;
+				return;
+			}
+		} catch(e:Dynamic) {
+			trace(e);
+			return;
+		}
+
+        Lua_helper.add_callback(lua, "noteTweenScale", function(tag:String, note:Int, value:Dynamic, duration:Float, ease:String) {
+			cancelTween(tag);
+			if(note < 0) note = 0;
+			var testicle:StrumNote = PlayState.instance.strumLineNotes.members[note % PlayState.instance.strumLineNotes.length];
+
+			if(testicle != null) {
+				PlayState.instance.modchartTweens.set(tag, FlxTween.tween(testicle, {scale: value}, duration, {ease: getFlxEaseByString(ease),
+					onComplete: function(twn:FlxTween) {
+						PlayState.instance.callOnLuas('onTweenCompleted', [tag]);
+						PlayState.instance.modchartTweens.remove(tag);
+					}
+				}));
+			}
+		});
+    }
+}
